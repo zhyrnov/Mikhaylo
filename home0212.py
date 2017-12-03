11ef8429e03c3a819da91bf20bae629cfba6e359
zhyrnov <m_zhyrnov@ukr.net>
2017-12-03 02:28:02 +0200

    dz0212

---
+++ home0212.py
@@ -0,0 +1,51 @@
+import math
+num1 = float(input('enter number 1='))
+num2 = float(input('enter number 2='))
+print('--------'*10)
+print('num1=',num1,'num2=',num2)
+print('--------'*10)
+# два числа ввели
+print ('''act Menu
+1. /
+2. * 
+3. - 
+4. +
+5. **
+6. con(x)
+7. sin(x)
+''')
+print('--------'*10)
+choice = int(input('your choice:'))
+print('--------'*10)
+if choice == 1:
+    print(num1,'/',num2,'=')
+    print (round((num1 / num2),3))
+elif choice == 2:
+    print(num1, '*', num2, '=')
+    print(round((num1 * num2),3))
+
+elif choice == 3:
+    print(num1, '-', num2, '=')
+    print(round((num1 - num2), 3))
+
+elif choice == 4:
+    print(num1, '+', num2, '=')
+    print(round((num1 + num2), 3))
+
+elif choice == 5:
+    print(num1, '**', num2, '=')
+    print(round((num1 ** num2), 3))
+
+elif choice == 6:
+    print(num1, 'cos', num2, '=')
+    y = math.cos(num1 + num2)
+    print(round(y, 3))
+
+elif choice == 7:
+    print(num1, 'sin', num2, '=')
+    x = math.sin(num1 + num2)
+    print(round(x,3))
+else:
+    print('invalid choice...'
+          'try again...'
+          'choice main menu!')
