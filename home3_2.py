4d312e7157fa547a71daf4201dfc5407a09901f3
zhyrnov <m_zhyrnov@ukr.net>
2017-12-10 13:28:08 +0200

    home 3 final

--- home3_2.py
+++ home3_2.py
@@ -1,10 +1,50 @@
 # Вывести на экран фигуры со звездочек
 # (Ромб, Елочка, Треугольник, Квадрат, ступеньки)
-
-
 a = int(input('longest = '))
-b = int(input('higest = '))
+
+
+def jcb():
+    for i in range(a):
+        print('*' * a, )
+
+
+def qwe():
+    for i in range(a):
+        print('*' * i)
+
+
+def asd():
+    for i in range(a):
+        print('*' * (a - i))
+
+
+# квадрат
+jcb()
+print('kvadrat')
+# треугольник
+qwe()
+print('treugolnik 1')
+asd()
+print('treugolnik 2')
+
+
+# Ромб
+def zxc():
+    print('*' * (a - i), end='')
+
+
+def cxz():
+    print('*' * i, end='')
+
 
 for i in range(a):
-    print('*' * i)
-
+    print(' ' * (a-i), end='')
+    cxz()
+    cxz()
+    print(' ' * (a-i))
+for i in range(a):
+    print(' ' * i, end='')
+    zxc()
+    zxc()
+    print(' ' * i)
+print('romb')
