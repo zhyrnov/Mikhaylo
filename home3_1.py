5efae1487721e6fd78a0d9deb4237db5297c6f9f
zhyrnov <m_zhyrnov@ukr.net>
2018-01-08 18:39:47 +0200

    final

--- home3_1.py
+++ home3_1.py
@@ -3,12 +3,8 @@
 a = 1
 b =1
 
-while True:
+for i in range(1,10):
     for a in range(1,10):
         print(a, '*', b, '=', a * b)
-    if a == 9:
-        b += 1
-    if b == 10:
-            break
-
-#
+        if a == 9:
+            b += 1
